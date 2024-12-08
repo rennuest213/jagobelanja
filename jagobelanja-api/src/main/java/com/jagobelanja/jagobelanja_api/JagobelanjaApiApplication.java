b8b27ec58af7b27a0156519a8f2de7add8ae45a7
pramudyamk <pramudya.mahardhika@bni.co.id>
2024-12-08 21:17:35 +0700

    feat: initial project setup

---
+++ jagobelanja-api/src/main/java/com/jagobelanja/jagobelanja_api/JagobelanjaApiApplication.java
@@ -0,0 +1,13 @@
+package com.jagobelanja.jagobelanja_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagobelanjaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagobelanjaApiApplication.class, args);
+	}
+
+}
---
+++ jagobelanja-api/src/test/java/com/jagobelanja/jagobelanja_api/JagobelanjaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagobelanja.jagobelanja_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagobelanjaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
